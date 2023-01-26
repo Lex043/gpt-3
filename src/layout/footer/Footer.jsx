30056dc3bb27f738c9c346aea8f80dfa2b613007
Oyebamiji Alex <alexferanmi390@gmail.com>
2023-01-26 17:33:53 +0100

    feat: footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,12 +1,45 @@
 import "./Footer.scss";
 
+import logo from "../../assets/GPT-3.svg";
 const Footer = () => {
   return (
     <footer className="footer">
       <section className="footer__first__section">
         <h1>Do you want to step in to the future before others</h1>
         <button>Request Early Access</button>
+
+          <section className="footer__cont2">
+              <figure className="footer__logo">
+                <img src={logo} alt="GPT-3 logo"/>
+                <p className="footer__logo__cap">Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+              </figure>
+
+              <section className="footer__links">
+                  <ul>
+                      <li className="links">Links</li>
+                      <li>Overons</li>
+                      <li>Social Media</li>
+                      <li>Counters</li>
+                      <li>Contact</li>
+                  </ul>
+
+                  <ul>
+                      <li className="company">Company</li>
+                      <li>Terms & Conditions</li>
+                      <li>Privacy Policy</li>
+                      <li>Contact</li>
+                  </ul>
+
+                  <ul>
+                      <li className="get__in__touch">Get in touch</li>
+                      <li>Crechterwoord K12 182 DK Alknjkcb</li>
+                      <li>085-132567</li>
+                      <li>info@payme.net</li>
+                  </ul>
+              </section>
+          </section>
       </section>
+        <p>© 2021 GPT-3. All rights reserved.</p>
     </footer>
   );
 };
